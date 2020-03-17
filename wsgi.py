1e9f55d87ed384cc4276934bea7626fbdcece451
Maciej Janicki <maciej.janicki@helsinki.fi>
2020-03-17 22:22:49 +0200

    move similar runos to the top

--- wsgi.py
+++ wsgi.py
@@ -18,9 +18,24 @@ def show_diff():
 def show_runo():
     nro = request.args.get('nro', 1, type=str)
     result = ['<h1>{}</h1>'.format(nro)]
-    result.append('<table>')
     with pymysql.connect(**config.MYSQL_PARAMS) as db:
         runo = Runo.from_db_by_nro(db, nro, fmt='mysql')
+        result.append('<p>')
+        result.append('<b>Similar runos:</b>')
+        db.execute(
+            'SELECT so.nro, s.sim_al FROM so_sim_al s'
+            ' JOIN sources so ON so.so_id = s.so2_id'
+            ' WHERE s.so1_id = %s AND s.sim_al > 0.01'
+            ' ORDER BY s.sim_al DESC;',
+            (runo.so_id,))
+        simrunos_text = []
+        for nro_2, sim in db.fetchall():
+            simrunos_text.append(
+                '<a href="/runodiff?nro1={}&nro2={}">{}</a> ({} %)'\
+                .format(nro, nro_2, nro_2, round(sim*100)))
+        result.append(',\n'.join(simrunos_text))
+        result.append('</p>')
+        result.append('<table>')
         for key in sorted(runo.meta.keys()):
             result.append(
                 '<tr><td colspan="2"><small><b>{}:</b> {}</small></td></tr>'\
@@ -31,21 +46,6 @@ def show_runo():
                 result.append(
                     '<tr><td><sup><small>{}</small></sup></td><td>{}</td></tr>'\
                     .format(i, v.text))
-    result.append('</table>')
-    result.append('<h2>Similar runos</h2>')
-    result.append('<table>')
-    with pymysql.connect(**config.MYSQL_PARAMS) as db:
-        db.execute(
-            'SELECT so.nro, s.sim_al FROM so_sim_al s'
-            ' JOIN sources so ON so.so_id = s.so2_id'
-            ' WHERE s.so1_id = %s AND s.sim_al > 0.01'
-            ' ORDER BY s.sim_al DESC;',
-            (runo.so_id,))
-        for nro_2, sim in db.fetchall():
-            result.append(
-                '<tr><td><a href="/runodiff?nro1={}&nro2={}">{}</a></td>'
-                '<td align="right">&emsp;{} %</td>'
-                '</tr>'.format(nro, nro_2, nro_2, round(sim*100)))
     result.append('</table>')
     return '\n'.join(result)
 
