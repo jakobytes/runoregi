622649792fb14d24c1738f693d5d31d952719b11
Maciej Janicki <maciej.janicki@helsinki.fi>
2022-01-28 13:26:39 +0200

    Verse cluster view: add "see also" links
    The links links send to other clusters to which a certain verse is similar.

--- view/verse.py
+++ view/verse.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from flask import render_template
 import pymysql
 
@@ -8,9 +9,34 @@ from data import \
 from external import make_map_link
 
 
+def get_similar_verses(db, clust_id):
+    db.execute(
+        'SELECT'
+        '  min(v1.v_id), p2.nro, vp2.pos, v2.text, vcf2.freq'
+        ' FROM'
+        '   v_sim s'
+        '   JOIN verses v1 ON s.v1_id = v1.v_id'
+        '   JOIN v_clust vc1 ON s.v1_id = vc1.v_id'
+        '   JOIN verses v2 ON s.v2_id = v2.v_id'
+        '   JOIN v_clust vc2 ON s.v2_id = vc2.v_id'
+        '   JOIN v_clust_freq vcf2 ON vc2.clust_id = vcf2.clust_id'
+        '   JOIN verse_poem vp2 ON v2.v_id = vp2.v_id'
+        '   JOIN poems p2 ON vp2.p_id = p2.p_id'
+        ' WHERE'
+        '   vc1.clust_id = %s'
+        '   AND vc1.clust_id <> vc2.clust_id'
+        ' GROUP BY'
+        '   vc2.clust_id;',
+        (clust_id,))
+    results = defaultdict(lambda: list())
+    for r in db.fetchall():
+        results[r[0]].append((r[1], r[2], clean_special_chars(r[3]), r[4]))
+    return dict(results)
+
+
 def render(nro=None, pos=None, v_id=None, fmt='html'):
 
-    def _group_by_source(verses, smd):
+    def _group_by_source(verses, smd, simverses):
         '''
         Group the results of the verses query by source, i.e. convert
         per-verse tuples:
@@ -24,13 +50,19 @@ def render(nro=None, pos=None, v_id=None, fmt='html'):
         for p_id, pos, v_id, text in verses:
             if p_id == cur_p_id and results:
                 results[-1][1].append((pos, v_id, text))
+                if v_id in simverses:
+                    results[-1][7].extend(simverses[v_id])
+                    del simverses[v_id]
             else:
                 cur_p_id = p_id
                 results.append(
                     (smd[p_id].nro, [(pos, v_id, clean_special_chars(text))],
                      smd[p_id].title, smd[p_id].location,
                      smd[p_id].year, smd[p_id].collector,
-                     render_themes_tree(smd[p_id].themes)))
+                     render_themes_tree(smd[p_id].themes),
+                     list(simverses[v_id]) if v_id in simverses else list()))
+                if v_id in simverses:
+                    del simverses[v_id]
         return results
 
     with pymysql.connect(**config.MYSQL_PARAMS) as db:
@@ -58,16 +90,18 @@ def render(nro=None, pos=None, v_id=None, fmt='html'):
             ' WHERE vc.clust_id = %s'
             ' ORDER BY vp.p_id, vp.pos;',
             (clust_id,))
+        verses = list(db.fetchall())
 
     if fmt == 'csv':
         return render_csv([
             (smd[p_id].nro, pos, text, smd[p_id].location, smd[p_id].collector,
              '\n'.join(' > '.join(t[1] for t in tt if len(t) >= 2) \
                        for tt in smd[p_id].themes)) \
-            for p_id, pos, v_id, text in db.fetchall()],
+            for p_id, pos, v_id, text in verses],
             header=('nro', 'pos', 'text', 'location', 'collector', 'themes'))
     else:
-        verses = _group_by_source(db.fetchall(), smd)
+        simverses = get_similar_verses(db, clust_id)
+        verses_by_src = _group_by_source(verses, smd, simverses)
         map_lnk = make_map_link('verse', nro=nro, pos=pos)
         return render_template('verse.html', map_lnk=map_lnk, nro=nro, pos=pos,
-                               text=text, verses=verses)
+                               text=text, verses=verses_by_src)
