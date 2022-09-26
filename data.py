jakobytes/runoregi

commit 7d49727b482b5843f43c29bd04e8542f399eef9f

added new literary works

data.py
@@ -69,15 +69,10 @@ def get_structured_metadata(
             return 'SKVR {} {}'.format(osa, _id)
         elif nro.startswith('j'):
             return 'JR {}'.format(_id)
-        elif nro.startswith('kalevala'):
-            return 'Kalevala {}'.format(_id)
         elif nro.startswith('kt'):
             return 'Kanteletar {}:{}'.format(int(nro[2:4]), int(nro[4:]))
-        elif nro.startswith('kalevipoeg'):
-            num = re.sub('^0', '', nro.replace('kalevipoeg', '')).upper()
-            return 'Kalevipoeg {}'.format(num)
         else:
-            return _id
+            return '{} {}'.format(osa, _id)
 
     query_lst = [
         'SELECT DISTINCT poems.p_id, nro,',

templates/search_idx.html
@@ -50,7 +50,8 @@ or: <a href="/skvr-themes">Entire SKVR index on one page</a>
 <li/><a href="/theme?id={{ theme_id }}">{{ name }}</a>
 {% endfor %}
 </ul>
-<h3>Kalevala</h3>
+<h2>Additional literary works</h2>
+<h3>Kalevala (1849)</h3>
 <table>
 <tr>
 <td><a href="/poem?nro=kalevala01">I</a></td>
@@ -113,6 +114,50 @@ or: <a href="/skvr-themes">Entire SKVR index on one page</a>
 <td><a href="/poem?nro=kalevala50">L</a></td>
 </tr>
 </table>
+<h3>Wanha Kalevala (1835)</h3>
+<table>
+<tr>
+<td><a href="/poem?nro=vkalevala01">I</a></td>
+<td><a href="/poem?nro=vkalevala02">II</a></td>
+<td><a href="/poem?nro=vkalevala03">III</a></td>
+<td><a href="/poem?nro=vkalevala04">IV</a></td>
+<td><a href="/poem?nro=vkalevala05">V</a></td>
+<td><a href="/poem?nro=vkalevala06">VI</a></td>
+<td><a href="/poem?nro=vkalevala07">VII</a></td>
+<td><a href="/poem?nro=vkalevala08">VIII</a></td>
+<td><a href="/poem?nro=vkalevala09">IX</a></td>
+<td><a href="/poem?nro=vkalevala10">X</a></td>
+</tr>
+<tr>
+<td><a href="/poem?nro=vkalevala11">XI</a></td>
+<td><a href="/poem?nro=vkalevala12">XII</a></td>
+<td><a href="/poem?nro=vkalevala13">XIII</a></td>
+<td><a href="/poem?nro=vkalevala14">XIV</a></td>
+<td><a href="/poem?nro=vkalevala15">XV</a></td>
+<td><a href="/poem?nro=vkalevala16">XVI</a></td>
+<td><a href="/poem?nro=vkalevala17">XVII</a></td>
+<td><a href="/poem?nro=vkalevala18">XVIII</a></td>
+<td><a href="/poem?nro=vkalevala19">XIX</a></td>
+<td><a href="/poem?nro=vkalevala20">XX</a></td>
+</tr>
+<tr>
+<td><a href="/poem?nro=vkalevala21">XXI</a></td>
+<td><a href="/poem?nro=vkalevala22">XXII</a></td>
+<td><a href="/poem?nro=vkalevala23">XXIII</a></td>
+<td><a href="/poem?nro=vkalevala24">XXIV</a></td>
+<td><a href="/poem?nro=vkalevala25">XXV</a></td>
+<td><a href="/poem?nro=vkalevala26">XXVI</a></td>
+<td><a href="/poem?nro=vkalevala27">XXVII</a></td>
+<td><a href="/poem?nro=vkalevala28">XXVIII</a></td>
+<td><a href="/poem?nro=vkalevala29">XXIX</a></td>
+<td><a href="/poem?nro=vkalevala30">XXX</a></td>
+</tr>
+<tr>
+<td><a href="/poem?nro=vkalevala31">XXXI</a></td>
+<td><a href="/poem?nro=vkalevala32">XXXII</a></td>
+<td colspan = "8"><a href="/poem?nro=vkalevala33">Epilogue</a></td>
+</tr>
+</table>
 <h3>Kanteletar</h3>
 <ul>
 {% for theme_id, name in cat['kanteletar'] %}
@@ -149,6 +194,44 @@ or: <a href="/skvr-themes">Entire SKVR index on one page</a>
 <td><a href="/poem?nro=kalevipoeg18">XVIII</a></td>
 <td><a href="/poem?nro=kalevipoeg19">XIX</a></td>
 <td><a href="/poem?nro=kalevipoeg20">XX</a></td>
+</tr>
+</table>
+<h3>Ilo-laulu Jesuksesta</h3>
+<table>
+<tr>
+<td><a href="/poem?nro=ilj01">I</a></td>
+<td><a href="/poem?nro=ilj02">II</a></td>
+<td><a href="/poem?nro=ilj03">III</a></td>
+<td><a href="/poem?nro=ilj04">IV</a></td>
+<td><a href="/poem?nro=ilj05">V</a></td>
+<td><a href="/poem?nro=ilj06">VI</a></td>
+<td><a href="/poem?nro=ilj07">VII</a></td>
+<td><a href="/poem?nro=ilj08">VIII</a></td>
+<td><a href="/poem?nro=ilj09">IX</a></td>
+<td><a href="/poem?nro=ilj10">X</a></td>
+</tr>
+<tr>
+<td><a href="/poem?nro=ilj11">XI</a></td>
+<td><a href="/poem?nro=ilj12">XII</a></td>
+<td><a href="/poem?nro=ilj13">XIII</a></td>
+<td><a href="/poem?nro=ilj14">XIV</a></td>
+<td><a href="/poem?nro=ilj15">XV</a></td>
+<td><a href="/poem?nro=ilj16">XVI</a></td>
+<td><a href="/poem?nro=ilj17">XVII</a></td>
+<td><a href="/poem?nro=ilj18">XVIII</a></td>
+<td><a href="/poem?nro=ilj19">XIX</a></td>
+<td><a href="/poem?nro=ilj20">XX</a></td>
+</tr>
+<tr>
+<td><a href="/poem?nro=ilj21">XXI</a></td>
+<td><a href="/poem?nro=ilj22">XXII</a></td>
+<td><a href="/poem?nro=ilj23">XXIII</a></td>
+<td><a href="/poem?nro=ilj24">XXIV</a></td>
+<td><a href="/poem?nro=ilj25">XXV</a></td>
+<td><a href="/poem?nro=ilj26">XXVI</a></td>
+<td><a href="/poem?nro=ilj27">XXVII</a></td>
+<td><a href="/poem?nro=ilj28">XXVIII</a></td>
+<td colspan="2"><a href="/poem?nro=ilj29">Päätös</a></td>
 </tr>
 </table>
 <hr/>

commit 4c2e851a40ef32765885c991c9c577569e9d35f1

fix ERAB titles (broken by 7d49727b)

data.py
@@ -7,7 +7,8 @@ import re
 
 StructuredMetadata = \
     namedtuple('StructuredMetadata',
-               ['p_id', 'nro', 'title', 'location', 'collector', 'year', 'themes'])
+               ['p_id', 'nro', 'collection', 'title', 'location',
+                'collector', 'year', 'themes'])
 
 
 def clean_special_chars(text):
@@ -64,18 +65,22 @@ def get_structured_metadata(
         nro = None, title = True, location = True, collector = True,
         year = True, themes = True):
 
-    def _make_title(nro, osa, _id):
-        if nro.startswith('skvr'):
+    def _make_title(nro, osa, _id, collection):
+        if collection == 'skvr':
             return 'SKVR {} {}'.format(osa, _id)
-        elif nro.startswith('j'):
+        elif collection == 'erab':
+            return _id
+        elif collection == 'jr':
             return 'JR {}'.format(_id)
         elif nro.startswith('kt'):
             return 'Kanteletar {}:{}'.format(int(nro[2:4]), int(nro[4:]))
-        else:
+        elif collection == 'literary':
             return '{} {}'.format(osa, _id)
+        else:
+            return nro
 
     query_lst = [
-        'SELECT DISTINCT poems.p_id, nro,',
+        'SELECT DISTINCT poems.p_id, nro, collection,',
         ('rm_osa.value as osa, rm_id.value as id,' if title else '"", "",'),
         ('GROUP_CONCAT(DISTINCT IFNULL(CONCAT(l2.name, " — ", l.name), l.name)'
          ' SEPARATOR "; "),'\
@@ -137,12 +142,12 @@ def get_structured_metadata(
         query_lst.append('GROUP BY poems.p_id')
     db.execute(' '.join(query_lst))
     results = []
-    for p_id, nro, osa, _id, loc, col, year, themes_str in db.fetchall():
+    for p_id, nro, collection, osa, _id, loc, col, year, themes_str in db.fetchall():
         themes = sorted(
             [tuple(t.split('@')) for t in tt.split('::')] \
             for tt in themes_str.split(';;;'))
-        tit = _make_title(nro, osa, _id) if title else nro
-        results.append(StructuredMetadata(p_id, nro, tit, loc, col, year, themes))
+        tit = _make_title(nro, osa, _id, collection) if title else nro
+        results.append(StructuredMetadata(p_id, nro, collection, tit, loc, col, year, themes))
     return results
 
 