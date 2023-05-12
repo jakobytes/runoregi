669988d8f3b9bf23df16ecc33967eaa9f1a207f9
Maciej Janicki <maciej.janicki@helsinki.fi>
2023-05-12 11:34:17 +0300

    use boolean mode

--- data/search.py
+++ data/search.py
@@ -4,7 +4,7 @@ def search_verses(db, q):
         'SELECT nro, pos, text FROM verses'
         ' NATURAL JOIN verse_poem'
         ' NATURAL JOIN poems'
-        ' WHERE MATCH(text) AGAINST(%s);', (q,))
+        ' WHERE MATCH(text) AGAINST(%s IN BOOLEAN MODE);', (q,))
     result = db.fetchall()
     return result
 
@@ -18,7 +18,7 @@ def search_themes(db, q):
       '  LEFT OUTER JOIN themes t2 on t1.par_id = t2.t_id'
       '  LEFT OUTER JOIN themes t3 on t2.par_id = t3.t_id'
       '  LEFT OUTER JOIN themes t4 on t3.par_id = t4.t_id'
-      '  WHERE MATCH(t1.name, t1.description) AGAINST(%s);', (q,))
+      '  WHERE MATCH(t1.name, t1.description) AGAINST(%s IN BOOLEAN MODE);', (q,))
     result = [(r[3], r[4], r[5],
                [r[i] for i in range(3) if r[i]]) \
               for r in db.fetchall()]
@@ -30,7 +30,7 @@ def search_meta(db, q):
     db.execute(\
         'SELECT nro, field, value FROM raw_meta'
         ' NATURAL JOIN poems'
-        ' WHERE MATCH(value) AGAINST(%s);', (q,))
+        ' WHERE MATCH(value) AGAINST(%s IN BOOLEAN MODE);', (q,))
     result = db.fetchall()
     return result
 
