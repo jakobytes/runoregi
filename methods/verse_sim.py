ca4e9658e4beb988b6619105949abfa5b9345dae
Maciej Janicki <maciej.janicki@helsinki.fi>
2023-11-30 17:20:17 +0200

    On-the-fly verse similarity: lower the min. number of bigrams
    Allows for aligning very short lines in the poemdiff and multidiff views.

--- methods/verse_sim.py
+++ methods/verse_sim.py
@@ -5,7 +5,7 @@ from shortsim.ngrcos import vectorize
 def compute_verse_similarity(poems, threshold):
     verses = set((v.v_id, v.text_cl if v.text_cl is not None else '') \
                  for p in poems.values() for v in p.text if v.v_type == 'V')
-    v_ids, v_texts, ngr_ids, m = vectorize(verses)
+    v_ids, v_texts, ngr_ids, m = vectorize(verses, min_ngrams=3)
     sim = m.dot(m.T)
     sim[sim < threshold] = 0
     v_sim = defaultdict(dict)
