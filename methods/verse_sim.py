9b378bf0e0e8392574d5ef9734a9b8ce999ac095
Maciej Janicki <maciej.janicki@helsinki.fi>
2024-10-24 15:29:25 +0300

    Update methods.verse_sim to match the current shortsim
    hsci-r/shortsim@366cc3a simplified the arguments and returned values
    of `shortsim.ngrcos.vectorize()`.

--- methods/verse_sim.py
+++ methods/verse_sim.py
@@ -3,13 +3,13 @@ from shortsim.ngrcos import vectorize
 
 
 def compute_verse_similarity(poems, threshold):
-    verses = set((v.v_id, v.text_cl if v.text_cl is not None else '') \
-                 for p in poems.values() for v in p.text if v.v_type == 'V')
-    v_ids, v_texts, ngr_ids, m = vectorize(verses, min_ngrams=3)
+    verses = [v for p in poems.values() for v in p.text if v.v_type == 'V']
+    v_texts = [(v.text_cl if v.text_cl is not None else '') for v in verses]
+    m = vectorize(v_texts)
     sim = m.dot(m.T)
     sim[sim < threshold] = 0
     v_sim = defaultdict(dict)
     for i, j in list(zip(*sim.nonzero())):
-        v_sim[v_ids[i]][v_ids[j]] = float(sim[i,j])
+        v_sim[verses[i].v_id][verses[j].v_id] = float(sim[i,j])
     return v_sim
 
