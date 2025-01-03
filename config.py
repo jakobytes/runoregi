b2c77f4aa4030799f91af81f45312abcae270d32
Jakob Lindström <83701663+jakobytes@users.noreply.github.com>
2025-01-03 17:56:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import warnings
 
 MYSQL_PARAMS = {
     'host' : os.getenv('DB_HOST'),
-    'port' : int(os.getenv('DB_PORT')),
+    'port' : int(3306)),
     'user' : os.getenv('DB_USER'),
     'password' : os.getenv('DB_PASS'),
     'database' : os.getenv('DB_NAME')
